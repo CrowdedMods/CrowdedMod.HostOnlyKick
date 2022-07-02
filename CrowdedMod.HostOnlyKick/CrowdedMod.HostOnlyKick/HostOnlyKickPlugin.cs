789611b3a9787e30bc89301c8498f7b1cb28b13d
Galster-dev <46283010+Galster-dev@users.noreply.github.com>
2022-07-02 15:55:18 +0300

    Initial commit

---
+++ CrowdedMod.HostOnlyKick/CrowdedMod.HostOnlyKick/HostOnlyKickPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace CrowdedMod.HostOnlyKick;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class HostOnlyKickPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+    }
+}
---
+++ CrowdedMod.HostOnlyKick/CrowdedMod.HostOnlyKick/Patches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace CrowdedMod.HostOnlyKick;
+
+internal static class Patches
+{
+    [HarmonyPatch(typeof(VoteBanSystem), nameof(VoteBanSystem.AddVote))]
+    public static class VoteBanSystemAddVotePatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(VoteBanSystem), nameof(VoteBanSystem.CmdAddVote))]
+    public static class VoteBanSystemCmdAddVotePatch
+    {
+        private static bool Prefix([HarmonyArgument(0)] int clientId)
+        {
+            if (AmongUsClient.Instance.AmHost)
+            {
+                AmongUsClient.Instance.KickPlayer(clientId, false);
+            }
+
+            return false;
+        }
+    }
+}
