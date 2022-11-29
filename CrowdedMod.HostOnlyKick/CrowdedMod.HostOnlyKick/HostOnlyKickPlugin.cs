1efbb0af6da732a627aff79aeff47168adf43ae2
Galster-dev <46283010+Galster-dev@users.noreply.github.com>
2022-11-29 03:02:44 +0300

    Mark the mod host-only to avoid handshake issues

--- CrowdedMod.HostOnlyKick/CrowdedMod.HostOnlyKick/HostOnlyKickPlugin.cs
+++ CrowdedMod.HostOnlyKick/CrowdedMod.HostOnlyKick/HostOnlyKickPlugin.cs
@@ -2,12 +2,15 @@
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
 
 namespace CrowdedMod.HostOnlyKick;
 
 [BepInAutoPlugin]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnHost)]
 public partial class HostOnlyKickPlugin : BasePlugin
 {
     public Harmony Harmony { get; } = new(Id);
